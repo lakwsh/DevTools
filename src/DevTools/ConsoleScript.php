86dd83bc0d357cef24846de563f614a0a45cf198
lakwsh <uitony@126.com>
2017-12-05 03:10:25 +0800

    Update Sth

--- src/DevTools/ConsoleScript.php
+++ src/DevTools/ConsoleScript.php
@@ -1,10 +1,10 @@
 <?php
 	const VERSION='1.12.7';
-    date_default_timezone_set('Asia/Hong_Kong');
+	date_default_timezone_set('Asia/Hong_Kong');
 	$opts=getopt('',['make:','relative:','out:','entry:','stub:']);
 	if(!isset($opts['make'])){
 		echo '== PocketMine-MP DevTools-lakwsh CLI interface =='.PHP_EOL.PHP_EOL;
-		echo 'Usage: '. PHP_BINARY .' -dphar.readonly=0 '.$argv[0].' --make <sourceFolder1[,sourceFolder2[,sourceFolder3...]]> --relative <relativePath> --entry "relativeSourcePath.php" --out <pharName.phar>'.PHP_EOL;
+		echo 'Usage: '.PHP_BINARY.' -dphar.readonly=0 '.$argv[0].' --make <sourceFolder1[,sourceFolder2[,sourceFolder3...]]> --relative <relativePath> --entry "relativeSourcePath.php" --out <pharName.phar>'.PHP_EOL;
 		exit(0);
 	}
 	if(ini_get('phar.readonly')==1){
@@ -25,31 +25,35 @@
 		if(count($includedPaths)>1){
 			echo 'You must specify a relative path with --relative <path> to be able to include multiple directories'.PHP_EOL;
 			exit(1);
-		}else{$basePath=rtrim(str_replace("\\",'/',realpath(array_shift($includedPaths))),'/').'/';}
-	}else{$basePath=rtrim(str_replace("\\",'/',realpath($opts['relative'])),'/').'/';}
-    $includedPaths=array_filter(array_map(function(string $path) use ($basePath):string{return str_replace($basePath,'',$path);},$includedPaths),function(string $v):bool{return $v!=='';});
+		}else{
+			$basePath=rtrim(str_replace("\\",'/',realpath(array_shift($includedPaths))),'/').'/';
+		}
+	}else{
+		$basePath=rtrim(str_replace("\\",'/',realpath($opts['relative'])),'/').'/';
+	}
+	$includedPaths=array_filter(array_map(function(string $path) use ($basePath):string{return str_replace($basePath,'',$path);},$includedPaths),function(string $v):bool{return $v!=='';});
 	$pharName=$opts['out']??'DevTools-lakwsh_v'.date('Y-m-d').'.phar';
 	$stubPath=$opts['stub']??'stub.php';
 	if(!is_dir($basePath)){
 		echo $basePath.' is not a folder'.PHP_EOL;
 		exit(1);
 	}
-    echo PHP_EOL;
-    if(file_exists($pharName)){
-        echo $pharName.' already exists, overwriting...'.PHP_EOL;
-        @unlink($pharName);
-    }
-    echo 'Creating '.$pharName.'...'.PHP_EOL;
+	echo PHP_EOL;
+	if(file_exists($pharName)){
+		echo $pharName.' already exists, overwriting...'.PHP_EOL;
+		@unlink($pharName);
+	}
+	echo 'Creating '.$pharName.'...'.PHP_EOL;
 	$phar=new \Phar($pharName);
-    $start=microtime(true);
+	$start=microtime(true);
 	if(file_exists($basePath.$stubPath)){
 		echo 'Using stub '.$basePath.$stubPath.PHP_EOL;
 		$phar->setStub('<?php require("phar://".__FILE__."/'.$stubPath.'"); __HALT_COMPILER();');
 	}elseif(isset($opts['entry'])){
 		$realEntry=realpath($opts['entry']);
 		if($realEntry===false) exit('Entry point not found');
-    	$entry=addslashes(str_replace("\\",'/',$realEntry));
-    	$entry=str_replace($basePath,'',$entry);
+		$entry=addslashes(str_replace("\\",'/',$realEntry));
+		$entry=str_replace($basePath,'',$entry);
 		echo "Setting entry point to ".$entry.PHP_EOL;
 		$phar->setStub('<?php require("phar://".__FILE__."/'.$entry.'"); __HALT_COMPILER();');
 	}else{
@@ -59,33 +63,29 @@
 			echo "Missing entry point or plugin.yml\n";
 			exit(1);
 		}
-		$phar->setMetadata([
-			'name'=>$metadata['name'],
-			'version'=>$metadata['version'],
-			'main'=>$metadata['main'],
-			'api'=>$metadata['api'],
-			'depend'=>($metadata['depend']??''),
-			'description'=>($metadata['description']??''),
-			'authors'=>($metadata['authors']??''),
-			'website'=>($metadata['website']??''),
-			'creationDate'=>time()
-		]);
+		$phar->setMetadata(['name'=>$metadata['name'],'version'=>$metadata['version'],'main'=>$metadata['main'],'api'=>$metadata['api'],'depend'=>($metadata['depend']??''),'description'=>($metadata['description']??''),'authors'=>($metadata['authors']??''),'website'=>($metadata['website']??''),'creationDate'=>time()]);
 		$phar->setStub('<?php echo "PocketMine-MP plugin '.$metadata['name'].' v'.$metadata['version'].'\nThis file has been generated using DevTools-lakwsh v'.VERSION.' at '.date('r').'\n----------------\n";if(extension_loaded("phar")){$phar=new \Phar(__FILE__);foreach($phar->getMetadata() as $key=>$value){echo ucfirst($key).": ".(is_array($value)?implode(",",$value):$value)."\n";}} __HALT_COMPILER();');
 	}
 	$phar->setSignatureAlgorithm(\Phar::SHA1);
 	$phar->startBuffering();
+	echo 'Starting syntax check'.PHP_EOL;
+	$flag=false;
+	foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($basePath)) as $file){
+		if(substr($file,-4)!=='.php') continue;
+		system('php -l "'.$file.'"',$return);
+		if($return!==0) $flag=true;
+	}
+	if($flag) exit(1);
+	else echo 'Syntax check done.'.PHP_EOL;
 	echo 'Adding files...'.PHP_EOL;
-	function preg_quote_array(array $strings,string $delim=null):array{return array_map(function(string $str) use ($delim):string{return preg_quote($str,$delim);},$strings);}
-    $excludedSubstrings=['/.',$pharName];
-    $regex=sprintf('/^(?!.*(%s))^%s(%s).*/i',implode('|',preg_quote_array($excludedSubstrings,'/')),preg_quote($basePath,'/'),implode('|',preg_quote_array($includedPaths,'/')));
-    foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($basePath)) as $file){
-    	if(substr($file,-4)!=='.php') continue;
-	    system('php -l "'.$file.'"',$return);
-	    if($return!==0) exit(1);
-    }
-    $count=count($phar->buildFromDirectory($basePath,$regex));
-    echo 'Added '.$count.' files'.PHP_EOL;
+	$excludedSubstrings=['/.',$pharName];
+	$regex=sprintf('/^(?!.*(%s))^%s(%s).*/i',implode('|',preg_quote_array($excludedSubstrings,'/')),preg_quote($basePath,'/'),implode('|',preg_quote_array($includedPaths,'/')));
+	$count=count($phar->buildFromDirectory($basePath,$regex));
+	echo 'Added '.$count.' files'.PHP_EOL;
 	$phar->compressFiles(\Phar::GZ);
 	$phar->stopBuffering();
 	echo PHP_EOL.'Done in '.round(microtime(true)-$start,3).'s'.PHP_EOL;
 	exit(0);
+	function preg_quote_array(array $strings,string $delim=null):array{
+		return array_map(function(string $str) use ($delim):string{return preg_quote($str,$delim);},$strings);
+	}
